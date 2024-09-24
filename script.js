4a53d4acfd77f237daede2ce14f53ac49792b8a7
terijeli <terijeli36@gmail.com>
2024-09-24 13:25:34 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    const textElement = document.getElementById('text');
+    textElement.textContent = 'Tuesdays are always pretty, pink & cute <3';
+});
