4803504c395c4a0a9cdb89abe41bd420ef086952
Tereza Jelinkova <153749535+terijeli@users.noreply.github.com>
2024-10-03 14:54:42 +0200

    Add files via upload

---
+++ assignment 2/script.js
@@ -0,0 +1,4 @@
+document.getElementById('changeTextBtn').addEventListener('click', function() {
+    const textElement = document.getElementById('text');
+    textElement.textContent = 'Wednesdays are always pretty, pink & cute <3';
+});
